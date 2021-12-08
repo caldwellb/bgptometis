86748397a4aac36a888071895aaf70401c42b02f
Ben Caldwell <caldwellb@uchicago.edu>
2021-12-08 11:28:39 -0600

    Overwritten

--- bgptometis.hs
+++ bgptometis.hs
@@ -1,62 +1,84 @@
-import qualified Data.Map as Map
-import qualified Data.Map.Strict as Map.Strict
-import System.Environment
-import Data.IORef
-import qualified Data.Set as Set
-import System.Command
-import Data.List.Split
-import System.Directory
-import Data.Foldable
-import Data.Traversable
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+import Shelly
+import qualified Data.Text as T
 import Control.Monad
+import System.Directory
+import Data.Time
+import Data.List.Split
+import Data.IORef
+import qualified Data.Map as Map
+import Data.Int
 import Data.Char
 import System.ProgressBar
-import Data.Time
-import Data.Bifunctor
+import qualified Data.IntMap as T
+import System.Environment
+default (T.Text)
 
-type ASConnections = Map.Map Int (Map.Map Int Int)
+type ASN = Int32
+type ASConnections = Map.Map ASN (Map.Map ASN Int)
+
+type IPRange  = T.Text
+type IPRanges = [IPRange]
 
 type ASNData = Map.Map Int IPRanges
 
-type IPRange  = String 
-type IPRanges = [IPRange]
+addASNToMap :: ASNData -> (ASN, IPRange) -> IO ASNData
+addASNToMap oldmap (key, val) = undefined
 
-getWeight :: Ord k => k -> Map.Map k Int -> Int
-getWeight = Map.findWithDefault 0
-
-
-addWeight :: Ord k => k -> Int -> Map.Map k Int -> Map.Map k Int
-addWeight = Map.Strict.insertWith (+)
-
-
-parseLine :: String -> (ASNData, ASConnections)
+parseLine :: T.Text -> ((ASN, T.Text), ASConnections)
 parseLine str = let
-    splitData = splitOn "|" str
+    splitData = T.splitOn "|" str
     ipRange   = splitData !! 5
-    asnList   = read <$> filter (all isDigit) (words (splitData !! 6))
-    in (Map.singleton (last asnList) [ipRange], Map.map (`Map.singleton` 1) $ Map.fromList $ zip asnList (tail asnList))
+    asnList   = map (read . T.unpack) $ takeWhile (T.all isDigit) (T.words (splitData !! 6))
+    conMap    = Map.fromList . zip asnList $ zipWith Map.singleton (tail asnList) [1, 1..]
+    in ((last asnList, ipRange),  conMap)
 
-unionASNData :: ASNData -> ASNData -> ASNData
-unionASNData  = Map.unionWith  (++)
+ipRangeToInt :: IPRange -> Int
+ipRangeToInt range = 2 ^ (32 - read (T.unpack (T.splitOn "/" range !! 1)) :: Int)
 
-unionsASNData :: [ASNData] -> ASNData
-unionsASNData = Map.unionsWith (++)
+ipRangeWeight :: IPRanges -> Int
+ipRangeWeight = sum . map ipRangeToInt
+
+retrieveIPWeight:: Ord k => k -> Map.Map k IPRanges -> Int
+retrieveIPWeight k mymap = ipRangeWeight $ Map.findWithDefault [] k mymap
+
+openBGPFile :: FilePath -> IO T.Text
+openBGPFile file = 
+    let cmdArg = silently $ cmd "bgpdump" ["-q", "-M", T.pack file]
+        in shelly cmdArg
+
+isBZ2File :: FilePath -> Bool
+isBZ2File str = last (splitOn "." str) == "bz2"
+
+isTuesday :: FilePath -> Bool
+isTuesday str = let 
+    dateStr = splitOn "." (last $ splitOn "/" str) !! 1
+    year  = read (take 4 dateStr) 
+    month = read (take 2 (drop 4 dateStr)) 
+    day   = read (take 2 (drop 6 dateStr)) 
+    in
+        Tuesday == dayOfWeek (fromGregorian year month day)
+
+testLine :: T.Text
+testLine = "TABLE_DUMP|10/27/01 05:48:26|B|209.244.2.115|3356|3.0.0.0/8|3356 701 80|IGP"
+
+takeSameDay :: [FilePath] -> [FilePath]
+takeSameDay []          = []
+takeSameDay (file:rest) = file:takeWhile isSameDay rest
+    where
+        isSameDay x = splitOn "." file !! 1 == splitOn "." x !! 1 
+
+combineIPRanges :: IPRanges -> IO IPRanges
+combineIPRanges ranges = --undefined
+    let cmdArg = map (T.dropWhile isSpace) . T.lines <$> silently (run "netmask" ranges)
+        in shelly cmdArg
 
 unionASConnections :: ASConnections -> ASConnections -> ASConnections
 unionASConnections = Map.unionWith (Map.unionWith (+))
 
-unionsASConnections :: [ASConnections] -> ASConnections
-unionsASConnections = Map.unionsWith (Map.unionWith (+))
-
-handleSingleFile :: String -> IO (ASNData, ASConnections)
-handleSingleFile filename = do
-    input <- lines <$> openBGPFile filename
-    let parsedLines   = map parseLine input
-    let asnMaps       = unionsASNData       $ fst <$> parsedLines
-    let asConMaps     = unionsASConnections $ snd <$> parsedLines
-    return (asnMaps, asConMaps)
-
-formatASConnections :: Map.Map Int Int -> String
+formatASConnections :: Map.Map ASN Int -> String
 formatASConnections asmap = unwords (map (\(x,y) -> show x ++ ' ':show y) (Map.toAscList asmap))
 
 getFiles :: String -> String -> IO [FilePath]
@@ -65,45 +87,46 @@ getFiles month year =
         map (\x -> current ++ '/':year ++ '.':month ++ "/RIBS/" ++ x) <$> 
             getDirectoryContents (current ++ '/':year ++ '.':month ++ "/RIBS")
 
-isTextFile :: FilePath -> Bool
-isTextFile str = last (splitOn "." str) == "txt"
-
-isBZ2File :: FilePath -> Bool
-isBZ2File str = last (splitOn "." str) == "bz2"
-
-combineIPRanges :: IPRanges -> IO IPRanges
-combineIPRanges ranges = do
-    Stdout out <- cmd "netmask -c" ranges
-    return (map (dropWhile isSpace) . lines $ out)
-
-openBGPFile :: FilePath -> IO String
-openBGPFile filepath = do
-    Stdout out <- cmd ("bgpdump -M " ++ filepath)
-    return out
-
-ipRangeToInt :: IPRange -> Int
-ipRangeToInt range = 2 ^ (32 - read (splitOn "/" range !! 1))
-
-ipRangeWeight :: IPRanges -> Int
-ipRangeWeight = sum . map ipRangeToInt
-
-retrieveIPWeight:: Ord k => k -> Map.Map k IPRanges -> Int
-retrieveIPWeight k mymap = ipRangeWeight $ Map.findWithDefault [] k mymap
-
-isTuesday :: FilePath -> Bool
-isTuesday str = let 
-    dateStr = splitOn "." (last (splitOn "/" str)) !! 1
-    year  = read (take 4 dateStr) :: Integer
-    month = read (take 2 (drop 4 dateStr)) :: Int
-    day   = read (take 2 (drop 6 dateStr)) :: Int
-    in
-        Tuesday == dayOfWeek (fromGregorian year month day)
-
-takeSameDay :: [FilePath] -> [FilePath]
-takeSameDay []          = []
-takeSameDay (file:rest) = file:takeWhile isSameDay rest
-    where
-        isSameDay x = splitOn "." file !! 1 == splitOn "." x !! 1 
+bgpDumpMonthYear :: String -> String -> IO ()
+bgpDumpMonthYear month year = do
+    let fileQuals = takeSameDay . filter (\x -> isBZ2File x && isTuesday x)
+    approvedFiles <- fileQuals <$> getFiles month year
+    asnMap <- newIORef Map.empty
+    conMap <- newIORef Map.empty
+    forM_ approvedFiles $ \filename -> do 
+        putStrLn ("Opening BGP file " ++ filename ++ " using bgpdump")
+        filetext <- T.lines <$> openBGPFile filename
+        putStr "Convering file " >> putStr filename >> putStrLn " to map" 
+        pb <- newProgressBar defStyle 10 (Progress 0 (length filetext) ())
+        forM_ filetext $ \line -> do
+            let ((asn, ipdata), newConnections) = parseLine line
+            modifyIORef' asnMap (Map.insertWith (++) asn [ipdata])
+            modifyIORef' conMap (unionASConnections newConnections)
+            incProgress pb 1
+        putStrLn "Squashing ipdata using netmask..."
+        rawASNMap <- readIORef asnMap
+        pb <- newProgressBar defStyle 10 (Progress 0 (Map.size rawASNMap) ())
+        combinedASN <- mapM (combineIPRanges >=> (\x -> incProgress pb 1 >> return x)) rawASNMap
+        writeIORef asnMap combinedASN
+    conMapFinal <- readIORef conMap
+    asnMapFinal <- readIORef asnMap 
+    let n = length . Map.keys  $ conMapFinal
+    putStrLn (show n ++ " nodes found")
+    let m = length . concatMap Map.elems . Map.elems $ conMapFinal
+    putStrLn (show m ++ " edges found")
+    let outputName = "2001.10"
+    let outMetis   = outputName ++ "metis"
+    let outNodemap = outputName ++ ".nodemap"
+    let conMapLst = Map.toAscList conMapFinal
+    let headerString = show n ++ ' ':show m ++ " 011"
+    writeFile outMetis   headerString
+    writeFile outNodemap ""
+    putStrLn "Writing output"
+    pb <- newProgressBar defStyle 10 (Progress 0 (length conMapLst) ())
+    forM_ conMapLst $ \(asnNum, asnweights) -> do
+        appendFile outMetis ('\n':show (retrieveIPWeight asnNum asnMapFinal)  ++ formatASConnections asnweights)
+        appendFile outNodemap $ show asnNum ++ "\n"
+        incProgress pb 1
 
 usage :: String
 usage = "bgptometis month year while in the bgpdata file, currently does not recursively search for files."
@@ -113,29 +136,4 @@ main = do
     args   <- getArgs
     case args of
         (month:year:xs) -> do
-            allFilePaths        <- getFiles month year
-            let acceptedFiles = takeSameDay . filter isTuesday . filter isBZ2File $ allFilePaths
-            putStrLn "BGP Files found:"
-            for_ acceptedFiles $ \file -> do
-                putStrLn file
-            (rawASNMap, conMap) <- bimap unionsASNData unionsASConnections . unzip <$> forM acceptedFiles handleSingleFile
-            putStrLn "Combining AS IP Ranges"
-            pb1                 <- newProgressBar defStyle 10 (Progress 0 (length rawASNMap) ())
-            asnMap              <- mapM (\x -> incProgress pb1 1 >> combineIPRanges x) rawASNMap
-            let n = length . Map.keys  $ conMap
-            putStrLn (show n ++ " nodes found")
-            let m = length . concatMap Map.elems . Map.elems $ conMap
-            putStrLn (show m ++ " edges found")
-            let outputName = month ++ year
-            let outMetis   = outputName ++ ".metis"
-            let outNodemap = outputName ++ ".nodemap"
-            let headerString = show n ++ ' ':show m ++ " 011"
-            writeFile outMetis headerString
-            writeFile outNodemap ""
-            putStrLn "Formatting and writing output"
-            pb2                  <- newProgressBar defStyle 10 (Progress 0 n ())
-            for_ (Map.toAscList conMap) (\(asnNum, asnweights) -> do
-                incProgress pb2 1
-                appendFile outMetis ('\n':show (retrieveIPWeight asnNum asnMap)  ++ formatASConnections asnweights)
-                appendFile outNodemap $ show asnNum ++ "\n")
-        [] -> putStrLn usage
+            bgpDumpMonthYear month year
